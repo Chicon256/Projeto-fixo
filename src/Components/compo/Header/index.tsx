92e5d1547338fcd7c2609b72b039ad0a1c61c3e3
jeredy256 <145425163+jeredy256@users.noreply.github.com>
2024-04-28 14:10:47 -0300

    finalmente

--- src/Components/compo/Header/index.tsx
+++ src/Components/compo/Header/index.tsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return(
         <header className="pito">
-            <button><Link to={"*"}>error page</Link></button>
+            
             <h1>aki eo header</h1>
+            <button><Link to={"*"}>error page</Link></button>
         </header>
     )
 }
